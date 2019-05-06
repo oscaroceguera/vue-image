c7c4f2ab216760221cf8aaba787116312841051a
oscar <oscaroceguera@gmail.com>
2019-05-06 02:06:16 -0600

    Handling state with vuex

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='ui secondary pointing menu'>
+    <a class="item active">
+      Image Storage
+    </a>
+    <a class="item active">
+      Image Storage
+    </a>
+    <a class="item active">
+      Image Storage
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style>
+
+</style>
+
